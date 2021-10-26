cec25096de2c4828707a80246b6659b27940a07c
h2o-creator <hadysalah1455@gmail.com>
2021-10-26 21:46:37 +0200

    Combine reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import users from './users'
+import questions from './questions'
+import { loadingBarReducer } from 'react-redux-loading-bar'
+
+export default combineReducers({
+    users,
+    questions,
+    loadingBar: loadingBarReducer
+})
