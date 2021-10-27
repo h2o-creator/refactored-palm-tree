b2107311389dbd5842f55a29baabe789a92cc1ad
h2o-creator <hadysalah1455@gmail.com>
2021-10-27 18:07:20 +0200

    Fix data loading and getting state from store

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import handleReceiveInitialData from '../actions/receiveInitialData'
 import LoadingBar from 'react-redux-loading-bar'
@@ -7,20 +7,22 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Alert from 'react-bootstrap/Alert'
+import Spinner from 'react-bootstrap/Spinner'
 import 'bootstrap/dist/css/bootstrap.css'
 import { FaHeart, FaTree } from 'react-icons/fa'
 
 
-function App({ dispatch }) {
+function App({ dispatch, users, questions, loading }) {
 
-	useLayoutEffect(() => {
-		dispatch(handleReceiveInitialData())
+	useEffect(() => {
+	    async function loadData() {
+			await dispatch(handleReceiveInitialData())
+		}
+		loadData()
 	}, [dispatch])
 
 	return (
 		<>
-			<LoadingBar />
-
 			<Navbar bg='dark' variant='dark'>
 				<Container>
 					<Navbar.Brand>
@@ -31,24 +33,37 @@ function App({ dispatch }) {
 					</Navbar.Brand>
 				</Container>
 			</Navbar>
+			<LoadingBar />
 
 			<br />
 
-			<Container fluid>
-				<Row>
-					<Col>
-						<Alert variant='secondary'>Welcome to refactored-palm-tree! <FaHeart color='red' /></Alert>
-					</Col>
-				</Row>
-			</Container>
+			{(loading === 0) ? (
+				<Container fluid>
+					<Row>
+						<Col>
+							<Alert variant='secondary'>
+								Welcome to refactored-palm-tree! 
+								<FaHeart color='red' />
+							</Alert>
+						</Col>
+					</Row>
+				</Container>
+			) : (
+				<Container align='center'>
+					<Spinner animation="grow" style={{
+						margin: '200px'
+					}} />
+				</Container>
+			)}
 		</>
 	);
 }
 
-function mapStateToProps({ users, questions }) {
+function mapStateToProps({ users, questions, loadingBar }) {
 	return {
-		users,
-		questions,
+		users: Object.values(users),
+		questions: Object.values(questions),
+		loading: loadingBar.default
 	}
 }
 
