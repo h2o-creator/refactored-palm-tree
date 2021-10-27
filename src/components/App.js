2ebdbfadbe6b609c9fa22aabffefd00678d62af3
h2o-creator <hadysalah1455@gmail.com>
2021-10-27 16:32:42 +0200

    Create basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,46 @@ import React, { useLayoutEffect } from 'react'
 import { connect } from 'react-redux'
 import handleReceiveInitialData from '../actions/receiveInitialData'
 import LoadingBar from 'react-redux-loading-bar'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
+import 'bootstrap/dist/css/bootstrap.css'
+import { FaHeart, FaTree } from 'react-icons/fa'
 
-function App(props) {
-	const { dispatch } = props
+
+function App({ dispatch }) {
 
 	useLayoutEffect(() => {
 		dispatch(handleReceiveInitialData())
 	}, [dispatch])
 
 	return (
-		<div>
+		<>
 			<LoadingBar />
-			refactored-palm-tree
-		</div>
+
+			<Navbar bg='dark' variant='dark'>
+				<Container>
+					<Navbar.Brand>
+						<FaTree color='green' className='d-inline-block align-middle' style={{
+							marginRight: '10px'
+						}} />
+						<span className='align-middle'>Refactored Palm Tree</span>
+					</Navbar.Brand>
+				</Container>
+			</Navbar>
+
+			<br />
+
+			<Container fluid>
+				<Row>
+					<Col>
+						<Alert variant='secondary'>Welcome to refactored-palm-tree! <FaHeart color='red' /></Alert>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 }
 
