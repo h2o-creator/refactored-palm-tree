a0d3e591e65c622a1bda3419e7e22c6d69c5bbc3
h2o-creator <hadysalah1455@gmail.com>
2021-10-28 21:24:34 +0200

    Create question redirect-to link

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import { connect } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { FaArrowRight } from 'react-icons/fa'
 
-function Question({ author, optionOne, optionTwo, timestamp }) {
+function Question({ author, optionOne, optionTwo, timestamp, id }) {
     const dateConst = new Date(timestamp)
     const date = dateConst.toLocaleDateString('en-IL');
     const time = dateConst.toLocaleTimeString('en-US');
+
+    const history = useHistory()
 
     return (
         <>
             <Card style={{ padding: '20px' }}>
                 <Card.Header>
-                    <h4>{`Would you rather ${optionOne.text} (${optionOne.votes.length}) `} <span style={{ color: 'red' }}>||</span> {` (${optionTwo.votes.length}) ${optionTwo.text}? `}</h4>
+                    <h4 >{`Would you rather ${optionOne.text} (${optionOne.votes.length}) `} <span style={{ color: 'red' }}>||</span> {` (${optionTwo.votes.length}) ${optionTwo.text}? `}</h4>
                     <br />
-                    <p className='blockquote-footer'>{`Asked by ${author} on ${date} at ${time}`}</p>
+                    <p className='blockquote-footer'>
+                        {`Asked by ${author}`} 
+                        <br />
+                        {`${date} ${time}`}
+                    </p>
+                    <p style={{ float: 'right' }}>{`${optionOne.votes.length} voted for the first option, ${optionTwo.votes.length} voted for the second`}</p>
                 </Card.Header>
-                <Card.Body>
-                    <br />
-                    <Card.Text as='p' style={{ color: 'deepskyblue' }}>
-                        {`${optionOne.votes.length} voted for the first option, ${optionTwo.votes.length} voted for the second`}
-                    </Card.Text>
+                <Card.Body>                    
+                    <FaArrowRight size='50px' onClick={() => history.push(`/question/${id}`)} />
                 </Card.Body>
             </Card>
             <br />
@@ -29,9 +35,10 @@ function Question({ author, optionOne, optionTwo, timestamp }) {
 
 function mapStateToProps({ questions }, { questionId }) {
     const question = questions[questionId]
-    const { author, optionOne, optionTwo, timestamp } = question
+    const { author, optionOne, optionTwo, timestamp, id } = question
 
     return {
+        id,
         author,
         optionOne,
         optionTwo,
