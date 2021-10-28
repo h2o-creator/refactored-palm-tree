d69cb15b0d7e9f3e52903f476ab878609c082d31
h2o-creator <hadysalah1455@gmail.com>
2021-10-28 21:23:53 +0200

    Add routes, 404 redirects and questions page

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,10 @@ import Spinner from 'react-bootstrap/Spinner'
 import 'bootstrap/dist/css/bootstrap.css'
 import ConnectedDashboard from './Dashboard'
 import Navigation from './Navigation'
-import { Route } from 'react-router-dom'
+import { Route, Switch, useLocation, Link } from 'react-router-dom'
 import Footer from './Footer'
+import { FaSadTear, FaCoffee } from 'react-icons/fa'
+import ConnectedQuestionPage from './QuestionPage'
 
 function App({ dispatch, loading }) {
 
@@ -28,15 +30,19 @@ function App({ dispatch, loading }) {
 			<br />
 
 			{(loading === 0) ? (
-				<Route exact path='/' component={ConnectedDashboard} />
+				<Switch>
+					<Route exact path='/'>
+						<ConnectedDashboard />
+					</Route>
+					<Route path='/question/:id'>
+						<ConnectedQuestionPage />
+					</Route>
+					<Route path='*'>
+						<PageNotFound />
+					</Route>
+				</Switch>
 			) : (
-				<Container align='center' fluid>
-					<Spinner animation="grow" style={{
-						backgroundColor: 'white',
-						margin: '200px',
-						boxShadow: '0px 0px 25px deepskyblue'
-					}} />
-				</Container>
+				<ShowLoadingSpinner />
 			)}
 
 			<br />
@@ -44,6 +50,32 @@ function App({ dispatch, loading }) {
 			<Footer />
 		</>
 	);
+}
+
+function PageNotFound() {
+	const location = useLocation();
+	return (
+		<Container style={{ textAlign: 'center' }} className='col-6' fluid>
+			<FaSadTear size='500px' color='orange' />
+			<br />
+			<h4>Requested location does not exist</h4>
+			<p>Couldn't find the requested path: {location.pathname}</p>
+			<Link to='/'>Grab a coffee, and return to home <FaCoffee /></Link>
+			<p>If you still think this was an error, kindly let us know by contacting us.</p>
+		</Container>
+	)
+}
+
+function ShowLoadingSpinner() {
+	return (
+		<Container align='center' fluid>
+			<Spinner animation="grow" style={{
+				backgroundColor: 'white',
+				margin: '200px',
+				boxShadow: '0px 0px 25px deepskyblue'
+			}} />
+		</Container>
+	)
 }
 
 function mapStateToProps({ loadingBar }) {
