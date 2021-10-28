98f3ef60074e1952812407c6b46560a0e817ae2c
h2o-creator <hadysalah1455@gmail.com>
2021-10-28 14:56:21 +0200

    Export components to individual files, use routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,14 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import handleReceiveInitialData from '../actions/receiveInitialData'
 import LoadingBar from 'react-redux-loading-bar'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Alert from 'react-bootstrap/Alert'
 import Spinner from 'react-bootstrap/Spinner'
 import 'bootstrap/dist/css/bootstrap.css'
-import { FaHeart, FaTree } from 'react-icons/fa'
-import ConnectedLogin from './Login'
+import ConnectedDashboard from './Dashboard'
+import Navigation from './Navigation'
+import { Route } from 'react-router-dom'
 
-function App({ dispatch, loading, authedUser, users }) {
+function App({ dispatch, loading }) {
 
 	useEffect(() => {
 	    async function loadData() {
@@ -24,62 +20,14 @@ function App({ dispatch, loading, authedUser, users }) {
 
 	return (
 		<>
-			<Navbar bg='dark' variant='dark'>
-				<Navbar.Brand>
-					<FaTree color='green' style={{
-						marginRight: '10px'
-					}} className='align-middle' />
-					<span className='align-middle'>Refactored Palm Tree</span>
-				</Navbar.Brand>
-				<Navbar.Collapse>
-					<Nav className='me-auto'>
-						<Nav.Link className='align-middle'>
-							Home
-						</Nav.Link>
-					</Nav>
-					{
-						(loading === 0 && authedUser !== null) && (
-							<Nav>
-								<Nav.Link className='align-middle'>
-									<img width='50px' style={{ borderRadius: '500px', border: '1px solid white', padding: '1px', margin: '5px' }} 
-										src={users.filter((user) => user.id === authedUser).map((user) => user.avatarURL)} 
-										alt={`Displaying avatar of ${authedUser}`} />
-									{users.filter((user) => user.id === authedUser).map((user) => user.name)}
-								</Nav.Link>
-							</Nav>
-						)
-					}
-				</Navbar.Collapse>
-				</Navbar>
+			<Navigation />
+
 			<LoadingBar />
 
 			<br />
 
 			{(loading === 0) ? (
-				(authedUser !== null) ? (
-					<Container fluid>
-						<Row>
-							<Col>
-								<Alert variant='secondary'>
-									Welcome to refactored-palm-tree! 
-									<FaHeart color='red' />
-								</Alert>
-							</Col>
-						</Row>
-					</Container>
-				) : (
-					<Container fluid>
-						<Row>
-							<Col>
-								<Alert variant='warning'>
-									Please login to continue!
-								</Alert>
-							</Col>
-						</Row>
-						<br />
-						<ConnectedLogin />
-					</Container>
-				)
+				<Route exact path='/' component={ConnectedDashboard} />
 			) : (
 				<Container align='center' fluid>
 					<Spinner animation="grow" style={{
@@ -91,11 +39,9 @@ function App({ dispatch, loading, authedUser, users }) {
 	);
 }
 
-function mapStateToProps({ loadingBar, authedUser, users }) {
+function mapStateToProps({ loadingBar }) {
 	return {
 		loading: loadingBar.default,
-		users: Object.values(users),
-		authedUser,
 	}
 }
 
