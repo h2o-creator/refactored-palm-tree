6d0dabf7eef29188aa4307cdf3555d547b09ed70
h2o-creator <hadysalah1455@gmail.com>
2021-10-28 16:15:18 +0200

    Fix indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,33 +3,33 @@ import SparkMD5 from 'spark-md5'
 import standard from 'randomcolor'
 
 export function generateUID () {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+	return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
   
 export function generateUAvatar (id) {
-    let url 
-    squareicon({
-      id,
-      hasher: SparkMD5.hash,
-      scheme: standard
-    }, (rej, res) => {
-      url = res
-    })
-    return url
+	let url 
+	squareicon({
+	  id,
+	  hasher: SparkMD5.hash,
+	  scheme: standard
+	}, (rej, res) => {
+	  url = res
+	})
+	return url
 }
 
 export function formatQuestion ({ optionOneText, optionTwoText, author }) {
-    return {
-      id: generateUID(),
-      timestamp: Date.now(),
-      author,
-      optionOne: {
-        votes: [],
-        text: optionOneText,
-      },
-      optionTwo: {
-        votes: [],
-        text: optionTwoText,
-      }
-    }
+	return {
+		id: generateUID(),
+		timestamp: Date.now(),
+		author,
+		optionOne: {
+			votes: [],
+			text: optionOneText,
+		},
+		optionTwo: {
+			votes: [],
+			text: optionTwoText,
+		}
+	}
 }
