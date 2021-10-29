642b7b7453167044e5900ce042722765a6583cda
h2o-creator <hadysalah1455@gmail.com>
2021-10-29 20:29:15 +0200

    Improve login code and add avatar of selected user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import Col from 'react-bootstrap/Col'
 import Alert from 'react-bootstrap/Alert'
 import Container from 'react-bootstrap/Container'
@@ -10,33 +10,48 @@ import { FaUserCircle, FaArrowRight } from 'react-icons/fa'
 import { withRouter } from 'react-router-dom'
 
 function Login({ dispatch, users, history }) {
-    const selectRef = useRef(null)
+    const [selectedUser, setUser] = useState(undefined)
+    let userAvatar = useRef(null)
 
     function handleSubmit(e) {
         e.preventDefault();
-        const { current } = selectRef
-        const { value } = current
-        dispatch(setAuthedUser(value))
+        if (selectedUser === 'undefined' || selectedUser === undefined) {
+            return false
+        }
+        dispatch(setAuthedUser(selectedUser))
         history.push('/')
+    }
+
+    function triggerUpdate(e) {
+        setUser(e.target.value)
+        if (e.target.value === 'undefined') {
+            return userAvatar.current = null
+        }
+        users.filter((user) => user.id === (e.target.value)).map((user) => userAvatar.current = user.avatarURL)
     }
 
     return (
         <Container fluid>
             <Col lg='6' style={{ border: '1px solid black', padding: '25px', boxShadow: '0px 0px 5px black', textAlign: 'center' }}>
-                <FaUserCircle size='75px' color='deepskyblue' />
+                {userAvatar.current !== null ? (
+                    <img src={userAvatar.current} width={'150px'} alt={`Avatar of ${selectedUser}`}
+                         style={{ borderRadius: '500px', border: '1px solid white', padding: '1px', margin: '5px' }} />
+                ) : (<FaUserCircle size='150px' color='deepskyblue' />)}
                 <h2 style={{ textAlign: 'center' }}>Hello, Guest!</h2>
                 <Alert variant='warning'>
                     Please login to continue!
                 </Alert>
                 <Form onSubmit={(e) => handleSubmit(e)}>
                     <Form.Group>
-                        <Form.Select ref={selectRef} >{users.map((user) => (
-                            <option value={user.id} key={user.id}>
-                                {user.name}
-                            </option>
-                        ))}</Form.Select>
+                        <Form.Select value={selectedUser} onChange={triggerUpdate} >
+                            <option value='undefined'>Select username to login</option>
+                            {users.map((user) => (
+                                <option value={user.id} key={user.id}>
+                                    {user.name}
+                                </option>
+                            ))}</Form.Select>
                         <br />
-                        <Button type='submit' variant='primary'>
+                        <Button type='submit' variant='primary' disabled={selectedUser === 'undefined' || selectedUser === undefined}>
                             Login <FaArrowRight />
                         </Button>
                     </Form.Group>
