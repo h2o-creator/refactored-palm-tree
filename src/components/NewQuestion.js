a51a8929d7412364cb0cc7b9f4a883965b0612b0
h2o-creator <hadysalah1455@gmail.com>
2021-10-29 20:29:33 +0200

    Create basic structure of NewQuestion

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import { formatQuestion } from '../utils/helpers'
+
+function NewQuestion({ authedUser }) {
+    const [questionOptions, setOptions] = useState({ 
+        optionOne: '',
+        optionTwo: ''
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (questionOptions.optionOne === '' || questionOptions.optionTwo === '') return false
+        const questionData = formatQuestion({
+            optionOneText: questionOptions.optionOne, 
+            optionTwoText: questionOptions.optionTwo, 
+            author: authedUser
+        })
+    }
+
+    function handleChange(e) {
+        setOptions({
+            ...questionOptions,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <Container align='center' fluid>
+            <h3>Post a new Question</h3>
+            <p>Posting as: {authedUser}</p>
+            <br />
+            <Col lg='5' style={{ border: '1px solid black', padding: '25px', boxShadow: '0px 0px 5px black', textAlign: 'center' }} >
+                <Form onSubmit={handleSubmit}>
+                    <h4>Would You Rather</h4>
+                    <Form.Group>
+                        <Form.Label>First Option</Form.Label>
+                        <Form.Control name='optionOne' onChange={handleChange} value={questionOptions.optionOne} type='text' placeholder='What would you prefer first?'
+                            maxLength={100} />
+                        <br />
+                        <Form.Label>Second Option</Form.Label>
+                        <Form.Control name='optionTwo' onChange={handleChange} value={questionOptions.optionTwo} type='text' placeholder='What would you prefer second?' 
+                            maxLength={100} />
+                    </Form.Group>
+                    <br />
+                    <Button type='submit' disabled={questionOptions.optionOne === '' || questionOptions.optionTwo === ''}>Add Question</Button>
+                </Form>
+            </Col>
+        </Container>
+    )
+}
+
+function mapStateToProps({ authedUser }) {
+    return {
+        authedUser,
+    }
+}
+
+export default connect(mapStateToProps)(NewQuestion)
