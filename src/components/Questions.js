d535a924c40abe2c62b25239820c66cb5aa24db5
h2o-creator <hadysalah1455@gmail.com>
2021-10-30 18:11:58 +0200

    Improve toggle, order questions list per-user

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,15 +5,15 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Collapse from 'react-bootstrap/Collapse'
 import Button from 'react-bootstrap/Button'
-import { FaArrowDown } from 'react-icons/fa'
+import { FaArrowDown, FaArrowLeft } from 'react-icons/fa'
 
-function Questions({ questionIds, questions }) {
+function Questions({ questionIds, questions, authedUser }) {
     const [toggleUnanswered, setUnanswered] = useState(true)
     const [toggleAnswered, setAnswered] = useState(false)
-    const unanswered = questionIds.filter((questionId) => (questions[questionId].optionOne.votes.length === 0 &&
-        questions[questionId].optionTwo.votes.length === 0))
-    const answered = questionIds.filter((questionId) => (questions[questionId].optionOne.votes.length > 0 ||
-        questions[questionId].optionTwo.votes.length > 0))
+    const unanswered = questionIds.filter((questionId) => (!questions[questionId].optionOne.votes.includes(authedUser) &&
+    !questions[questionId].optionTwo.votes.includes(authedUser)))
+    const answered = questionIds.filter((questionId) => (questions[questionId].optionOne.votes.includes(authedUser) ||
+        questions[questionId].optionTwo.votes.includes(authedUser)))
     return (
         <>
             <Col className='col-12 col-lg-6'>
@@ -23,7 +23,9 @@ function Questions({ questionIds, questions }) {
                     </Col>
                     <Col className='col-2'>
                         <Button aria-expanded={toggleUnanswered} aria-controls='toggle-unanswered' onClick={() => setUnanswered(!toggleUnanswered)}>
-                            <FaArrowDown />
+                            {
+                                (toggleUnanswered === true ? <FaArrowDown /> : <FaArrowLeft />)
+                            }
                         </Button>
                     </Col>
                 </Row>
@@ -46,7 +48,9 @@ function Questions({ questionIds, questions }) {
                     </Col>
                     <Col className='col-2'>
                         <Button aria-expanded={toggleAnswered} aria-controls='toggle-answered' onClick={() => setAnswered(!toggleAnswered)}>
-                            <FaArrowDown />
+                            {
+                                (toggleAnswered === true ? <FaArrowDown /> : <FaArrowLeft />)
+                            }
                         </Button>
                     </Col>
                 </Row>
@@ -65,12 +69,12 @@ function Questions({ questionIds, questions }) {
     )
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, authedUser }) {
     return {
         questionIds: Object.keys(questions)
                         .sort((a, b) => (questions[b].timestamp - questions[a].timestamp)),
         questions,
-                    
+        authedUser,
     }
 }
 
