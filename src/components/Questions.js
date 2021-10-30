d908e73c81f68f9869ca3761b0f5b68ed243c5b8
h2o-creator <hadysalah1455@gmail.com>
2021-10-30 14:14:39 +0200

    Organize questions by answered, unanswered

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,20 +3,32 @@ import { connect } from 'react-redux'
 import ConnectedQuestion from './Question'
 import Col from 'react-bootstrap/Col'
 
-function Questions({ questionIds }) {
+function Questions({ questionIds, questions }) {
     return (
-        <Col>
-            {questionIds.map((questionId) => (
-                <ConnectedQuestion key={questionId} questionId={questionId} />
-            ))}
-        </Col>
+        <>
+            <Col>
+                <h3>Unanswered Questions</h3>
+                <hr />
+                {questionIds.filter((questionId) => (questions[questionId].optionOne.votes.length === 0 &&
+                    questions[questionId].optionTwo.votes.length === 0)).map((questionId) => (
+                    <ConnectedQuestion key={questionId} questionId={questionId} />
+                ))}
+                <h3>Answered Questions</h3>
+                <hr />
+                {questionIds.filter((questionId) => (questions[questionId].optionOne.votes.length > 0 ||
+                    questions[questionId].optionTwo.votes.length > 0)).map((questionId) => (
+                    <ConnectedQuestion key={questionId} questionId={questionId} />
+                ))}
+            </Col>
+        </>
     )
 }
 
 function mapStateToProps({ questions }) {
     return {
         questionIds: Object.keys(questions)
-                        .sort((a, b) => (questions[b].timestamp - questions[a].timestamp))
+                        .sort((a, b) => (questions[b].timestamp - questions[a].timestamp)),
+        questions,
                     
     }
 }
