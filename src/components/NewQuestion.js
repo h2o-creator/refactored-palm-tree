783f5ebbe17f12207e78877539f1aad74549c910
h2o-creator <hadysalah1455@gmail.com>
2021-10-30 12:53:25 +0200

    Improve new question layout

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -4,6 +4,8 @@ import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
 import handleAddQuestion from '../actions/addNewQuestion'
 import { useHistory } from 'react-router-dom'
 
@@ -35,35 +37,39 @@ function NewQuestion({ dispatch, authedUser }) {
     }
 
     return (
-        <Container align='center' className='custom-body' fluid>
-            <h3>Post a new Question</h3>
-            <p>Posting as: {authedUser}</p>
+        <Container className='custom-body' fluid>
+            <Row style={{ textAlign: 'center' }}>
+                <h3>Post a new Question</h3>
+                <p>Posting as: {authedUser}</p>
+            </Row>
             <br />
-            <Col lg='6' style={{ border: '1px solid black', padding: '25px', boxShadow: '0px 0px 5px black', textAlign: 'center' }} >
-                <Form onSubmit={handleSubmit}>
-                    <h4>Would You Rather</h4>
-                    <Form.Group>
-                        <Form.Label>First Option</Form.Label>
-                        <Form.Control name='optionOne' onChange={handleChange} value={questionOptions.optionOne} type='text' placeholder='What would you prefer first?'
-                            maxLength={100} />
-                        {
-                            (threshold - questionOptions.optionOne.length < 25) && (
-                                <span style={{ color: 'red', float: 'right' }}>{threshold - questionOptions.optionOne.length}</span>
-                            )
-                        }
+            <Col lg='6' style={{ margin: '0 auto' }}>
+                <Card style={{ padding: '25px' }}>
+                    <Form onSubmit={handleSubmit}>
+                        <h4>Would You Rather</h4>
+                        <Form.Group>
+                            <Form.Label>First Option</Form.Label>
+                            <Form.Control name='optionOne' onChange={handleChange} value={questionOptions.optionOne} type='text' placeholder='What would you prefer first?'
+                                maxLength={100} />
+                            {
+                                (threshold - questionOptions.optionOne.length < 25) && (
+                                    <span style={{ color: 'red', float: 'right' }}>{threshold - questionOptions.optionOne.length}</span>
+                                )
+                            }
+                            <br />
+                            <Form.Label>Second Option</Form.Label>
+                            <Form.Control name='optionTwo' onChange={handleChange} value={questionOptions.optionTwo} type='text' placeholder='What would you prefer second?' 
+                                maxLength={100} />
+                            {
+                                (threshold - questionOptions.optionTwo.length < 25) && (
+                                    <span style={{ color: 'red', float: 'right' }}>{threshold - questionOptions.optionTwo.length}</span>
+                                )
+                            }
+                        </Form.Group>
                         <br />
-                        <Form.Label>Second Option</Form.Label>
-                        <Form.Control name='optionTwo' onChange={handleChange} value={questionOptions.optionTwo} type='text' placeholder='What would you prefer second?' 
-                            maxLength={100} />
-                        {
-                            (threshold - questionOptions.optionTwo.length < 25) && (
-                                <span style={{ color: 'red', float: 'right' }}>{threshold - questionOptions.optionTwo.length}</span>
-                            )
-                        }
-                    </Form.Group>
-                    <br />
-                    <Button type='submit' disabled={questionOptions.optionOne === '' || questionOptions.optionTwo === ''}>Add Question</Button>
-                </Form>
+                        <Button type='submit' disabled={questionOptions.optionOne === '' || questionOptions.optionTwo === ''}>Add Question</Button>
+                    </Form>
+                </Card>
             </Col>
         </Container>
     )
